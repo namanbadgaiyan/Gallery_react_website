696b0ab2a38a5ecfa1cf6869298b5ed66c6544f8
naman badgaiyan <namanbadgaiyan03@gmail.com>
2024-07-18 23:54:25 +0530

    color palette  integrated

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 
 const Nav = () => {
     return (
-        <div className="flex items-center justify-center w-full py-5 bg-[#28A013] text-[#323233] px-[2vh]">
+        <div className="flex items-center justify-center w-full py-5 bg-[#0D3B66] text-[#FAF0CA] px-[2vh]">
             <h1 className="text-3xl font-bold capitalize">search from gallery online</h1>
         </div>
     )
